77978ab2169bebd043867420fe4042bee48806b9
Rais Kazi <meetrais@gmail.com>
2025-06-29 10:07:46 -0500

    Modified code.

--- run_model.py
+++ run_model.py
@@ -1,23 +1,30 @@
-# Install the following dependencies: azure.identity and azure-ai-inference
 import os
 from azure.ai.inference import ChatCompletionsClient
 from azure.ai.inference.models import SystemMessage, UserMessage
 from azure.core.credentials import AzureKeyCredential
+from dotenv import load_dotenv
 
-endpoint = os.getenv("AZURE_INFERENCE_SDK_ENDPOINT", "https://ai-genaihub887076259506.services.ai.azure.com/models")
-deployment_name = "Ministral-3B"
-key = os.getenv("AZURE_INFERENCE_SDK_KEY", "YOUR_KEY_HERE")
-client = ChatCompletionsClient(endpoint=endpoint, credential=AzureKeyCredential(key))
+load_dotenv()
 
-response = client.complete(
-  messages=[
-    SystemMessage(content="You are a helpful assistant."),
-    UserMessage(content="What are 3 things to visit in Seattle?")
-  ],
-  max_tokens=2048,
-    temperature=0.8,
-    top_p=0.1,
-    model = deployment_name,
+endpoint = os.getenv("AZURE_EXISTING_AIPROJECT_ENDPOINT")
+api_key = os.getenv("API_KEY")
+model_name = "Ministral-3B"
+
+client = ChatCompletionsClient(
+    endpoint=endpoint,
+    credential=AzureKeyCredential(api_key),
+    api_version="2024-05-01-preview"
 )
 
-print(response)
+response = client.complete(
+    messages=[
+        SystemMessage(content="You are a helpful assistant."),
+        UserMessage(content="I am going to Paris, what should I see?"),
+    ],
+    max_tokens=2048,
+    temperature=0.8,
+    top_p=0.1,
+    model=model_name
+)
+
+print(response.choices[0].message.content)
